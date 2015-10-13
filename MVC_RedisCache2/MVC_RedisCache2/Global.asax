b5d3aeaa1be5fd55dfec8b5b823aff4cd2243a42
felix <felix@stebra.se>
2015-10-13 10:29:48 +0200

    Initial Commit, working example

---
+++ MVC_RedisCache2/MVC_RedisCache2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_RedisCache2.MvcApplication" Language="C#" %>
---
+++ MVC_RedisCache2/MVC_RedisCache2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_RedisCache2.Startup))]
+namespace MVC_RedisCache2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
