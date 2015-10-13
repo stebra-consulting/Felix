5f395110d95217d86486e85971aa4a6582346e57
felix <felix@stebra.se>
2015-10-13 10:58:30 +0200

    Initial Commit Public Azure Website with redisCache reference/code

---
+++ MAA_Public/MAA_Public/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StackExchange.Redis;
+
+namespace MAA_Public.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(
+                "memory.redis.cache.windows.net,abortConnect=false,ssl=true,password=nv2w3Oz+20mPCR2vVQVLCwUc3PAYpokJy02LK0JcEHQ=");
+
+            IDatabase cache = connection.GetDatabase();
+
+            // Perform cache operations using the cache object...
+            // Simple put of integral data types into the cache
+            //cache.StringSet("key1", "value");
+            //cache.StringSet("key2", 25);
+
+            // Simple get of data types from the cache
+            string key1 = cache.StringGet("key1");
+            int key2 = (int)cache.StringGet("key2");
+
+            ViewBag.key1 = key1;
+            ViewBag.key2 = key2;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MAA_Public/MAA_Public/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MAA_Public.MvcApplication" Language="C#" %>
