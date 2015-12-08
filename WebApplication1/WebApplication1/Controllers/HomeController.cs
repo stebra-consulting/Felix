aceb2301027fe31f41d8ce6fc4211fcd8d5bd2c2
felix <felix@stebra.se>
2015-12-08 12:35:59 +0100

    added solution Webapplication1 to test azure table access

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            AzureManager.SelectTable();
+
+            //List<ListItem> stebraList = new List<ListItem>();
+            //AzureManager.CreateTable(stebraList);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
